e4279213d6e6b376255e1a9f642dab40e6a75dd1
Adnen HAMDOUNI <adnen.hamdouni@squeezer-software.com>
2016-12-14 11:43:10 +0100

    fix posts message and base activity

--- app/src/main/java/tn/itskills/android/isgc_firebase/BaseActivity.java
+++ app/src/main/java/tn/itskills/android/isgc_firebase/BaseActivity.java
@@ -3,6 +3,7 @@ package tn.itskills.android.isgc_firebase;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 /**
@@ -33,14 +34,14 @@ public class BaseActivity extends AppCompatActivity {
     * getUid from firebaseAuth instance
      */
     public String getUid() {
-        return null;
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
     /*
     * getCurrentUser from firebaseAuth instance
     */
     public FirebaseUser getUser() {
-        return null;
+        return FirebaseAuth.getInstance().getCurrentUser();
     }
 
 }
