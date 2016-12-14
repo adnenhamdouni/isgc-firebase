4164823c481109d2e150df1b232d88e5a1f13400
Adnen HAMDOUNI <adnen.hamdouni@squeezer-software.com>
2016-12-14 11:12:05 +0100

    fix logout action

--- app/src/main/java/tn/itskills/android/isgc_firebase/HomeActivity.java
+++ app/src/main/java/tn/itskills/android/isgc_firebase/HomeActivity.java
@@ -133,7 +133,7 @@ public class HomeActivity extends AppCompatActivity implements ValueEventListene
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
             signOut();
             return true;
         }
